df95d9fe7516fb93b95edce267f3a04d6c316fab
Surya <sxrya007@gmail.com>
2025-05-02 13:27:23 +0530

    DBConnection.java

--- Nestify_Market/src/main/java/org/example/DBConnection.java
+++ Nestify_Market/src/main/java/org/example/DBConnection.java
@@ -4,9 +4,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/nestify_market";
-    private static final String USER = "root";
-    private static final String PASSWORD = "surya@2003";
+    private static final String URL = "jdbc:mysql://localhost:3306/db_name";
+    private static final String USER = "user name";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
